54519bd01ed9c88151b486b8c22b91d09ea8ecdb
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-14 10:47:57 +0000

    Setup main contents

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,11 @@
+import UserInputs from './UserInputs'
+import CVPreviewer from './CVPreviewer'
+
 export default function Main () {
-  return <main className='flex-grow-1' />
+  return (
+    <main className='flex-grow-1'>
+      <UserInputs />
+      <CVPreviewer />
+    </main>
+  )
 }
