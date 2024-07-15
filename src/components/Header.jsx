6c89bb212b30bd91e94151fc8285cffb76daa499
GDA0 <gideondelaliadeti0@gmail.com>
2024-07-15 05:01:45 +0000

    Add border bottom to header nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header () {
   return (
     <header>
-      <nav className='navbar fixed-top bg-white container'>
-        <div className='container-fluid'>
+      <nav className='navbar fixed-top bg-white container-fluid border-bottom border-2'>
+        <div className='container'>
           <span className='navbar-brand mb-0 h1'>CV Application</span>
         </div>
       </nav>
